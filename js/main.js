7f11c0e6af8d1dd010f2c9a49fdddf5965702b0a
Edvin Flodström <edvin.flodstrom@gmail.com>
2024-04-19 19:55:26 +0200

    feat(all): Add some button logic

--- js/main.js
+++ js/main.js
@@ -4,10 +4,37 @@ const form = document.querySelector('#numberOfButtonsForm');
 const numberInput = document.querySelector('#numberInput');
 const submitButton = form.querySelector('button[type="submit"]');
 
+const maxNumberOfButtons = 20;
+
 numberInput.addEventListener('input', () => {
-    ValidateNumberInput(numberInput.value);
+    const numberInputValue = Number(numberInput.value);
+    if (numberInputValue > 0 && numberInputValue <= maxNumberOfButtons) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
 });
 
-function ValidateNumberInput(number) {
-    console.log(`Hello, World! ${number}`);
+class GeneratedButton {
+    constructor(xPos, yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
 }
+
+function generateNewButton(buttonInstance) {
+    const button = document.createElement('button');
+    button.style.position = 'absolute';
+    button.style.left = buttonInstance.xPos;
+    button.style.top = buttonInstance.yPos;
+    button.addEventListener('click', () => {
+        console.log("Hello, World!");
+        button.disabled = true;
+    });
+    document.body.appendChild(button);
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    generateNewButton(new GeneratedButton(30, 30));
+})
